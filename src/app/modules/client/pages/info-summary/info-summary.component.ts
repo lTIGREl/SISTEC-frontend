8119ba6862dea73db65d5a1efc81d2a12f3c938e
lTIGREl <joefernando20899@gmail.com>
2024-07-25 15:00:37 -0500

    Project created
    Initial folder project structure
    Two pages for register client and for show info

---
+++ src/app/modules/client/pages/info-summary/info-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './info-summary.component.html',
+  styleUrl: './info-summary.component.scss'
+})
+export class InfoSummaryComponent {
+
+}
